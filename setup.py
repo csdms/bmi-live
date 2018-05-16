0bb11618466d5277989e61419054ff8be7e47e41
Mark Piper <mark.piper@colorado.edu>
2018-05-15 20:50:22 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(name='bmi-live-2018',
+      version='0.1',
+      author='Mark Piper',
+      author_email='mark.piper@colorado.edu',
+      license='MIT',
+      description='Source for the 2018 BMI Live clinic',
+      long_description=open('README.md').read(),
+      install_requires=('numpy', 'pyyaml'),
+      packages=find_packages(exclude=['*.tests']),
+)
