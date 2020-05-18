6a1d9a327ebf64acba9bd02330bfa047e8137337
Mark Piper <mark.piper@colorado.edu>
2020-05-18 15:40:20 -0600

    Include classes in package definition

--- bmi_live/__init__.py
+++ bmi_live/__init__.py
@@ -1,6 +1,10 @@
 """BMI Live clinic"""
 import os
+from .diffusion import Diffusion
+from .bmi_diffusion import BmiDiffusion
 
+
+__all__ = ['Diffusion', 'BmiDiffusion']
 
 pkg_directory = os.path.dirname(__file__)
 data_directory = os.path.join(pkg_directory, 'data')
